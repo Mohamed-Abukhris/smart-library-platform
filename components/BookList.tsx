8d8c9dab7c754013150d01044398ff07fff84fad
Mohamed-Abukhris <mohamed.s.abukhris@gmail.com>
2025-10-25 18:27:47 +0200

    feat: implement BookCard component and update BookList to display latest books

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BookCard from "@/components/BookCard";
 
 interface Props {
   title: string;
@@ -6,10 +7,16 @@ interface Props {
   containerClassName?: string;
 }
 
-const BookList = ({ title, boooks, containerClassName }: Props) => {
+const BookList = ({ title, books, containerClassName }: Props) => {
   return (
-    <section>
-      <h2 className="font-bebas-neue text-4xl text-light-100">Popular Books</h2>
+    <section className={containerClassName}>
+      <h2 className="font-bebas-neue text-4xl text-light-100">{title}</h2>
+
+      <ul className="book-list">
+        {books.map((book) => (
+          <BookCard key={book.title} {...book} />
+        ))}
+      </ul>
     </section>
   );
 };
